41dac441dc87dd5191caac1c597472652a1417f2
chenxq <chenxq2018@outlook.com>
2023-07-26 13:04:36 +0800

    add ant

---
+++ ant-design-last/src/locales/en.ts
@@ -0,0 +1,39 @@
+export default {
+  route: {
+
+    About: 'About111',
+    Index: 'Index111',
+    IndexIndex: 'IndexIndex111',
+
+
+
+    Login: 'Sign in',
+
+    Dashboard: 'workbench',
+    DashboardHome: 'home page',
+    DashboardCneter: 'personal information',
+
+    Self: 'Household items',
+    ArticleList: 'All items',
+    SelfVip: 'Membership records',
+    SelfPeriod: 'quality guarantee period',
+    SelfPeriodform: 'Shelf life - form',
+    SelfConsume: 'Consumption List',
+
+    Lottery: 'Entertainment Football',
+    LotteryList: 'List of lottery purchases',
+    LotteryForm: 'Lottery Purchase Form',
+    LotterySoccer: "2023 Women's Football World Cup",
+    LotterySsq: 'Bicolor sphere',
+    LotteryDlt: 'Grand Lotto',
+
+    Utils: 'tool',
+    UtilsCrypto: 'Encryption and decryption',
+  },
+  subpages: {
+    pageNoFound: 'Page not found',
+    desic: 'Sorry, we couldn’t find the page you’re looking for.',
+    backHome: 'Go back home',
+    support: 'Contact support'
+  }
+}
